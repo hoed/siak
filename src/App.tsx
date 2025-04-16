258dbace5cd80129f744077b632121f9dd279a20
hoedhud <hoedhud@gmail.com>
2025-04-16 22:39:03 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, RequireAuth } from '@/contexts/auth';
-import { Analytics } from '@vercel/analytics/react';
 
 // Pages
 import Login from '@/pages/Login';
@@ -50,7 +49,6 @@ const App: React.FC = () => {
             </Routes>
           </AuthProvider>
         </BrowserRouter>
-        <Analytics />
       </TooltipProvider>
     </QueryClientProvider>
   );
