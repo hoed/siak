ba90b903127a710c2251a185b64170f2e4ded783
hoedhud <hoedhud@gmail.com>
2025-04-16 22:54:44 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, RequireAuth } from '@/contexts/auth';
+import { Analytics } from "@vercel/analytics/react"
 
 // Pages
 import Login from '@/pages/Login';
